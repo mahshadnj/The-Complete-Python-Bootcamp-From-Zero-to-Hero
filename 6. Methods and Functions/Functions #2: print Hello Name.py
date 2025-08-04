4578761efeb66b14800c5252bf26455b589e5fe6
Mahshad Nazari Jeirani <88314947+mahshadnj@users.noreply.github.com>
2025-08-04 10:29:17 +0330

    Create Functions #2: print Hello Name.py

---
+++ 6. Methods and Functions/Functions #2: print Hello Name.py
@@ -0,0 +1,9 @@
+'''
+Define a function called myfunc that takes in a name, and prints 'Hello Name' 
+NOTE: Define the function, but do not run it!
+Also, do not use f-strings, as they are not supported in this Coding Exercise.
+'''
+
+def myfunc(name):
+    print("Hello {}".format(name))
+    
