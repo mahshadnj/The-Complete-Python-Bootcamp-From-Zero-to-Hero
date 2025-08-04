589bda38d23ea60af8073125f2148327b6fc4b0e
Mahshad Nazari Jeirani <88314947+mahshadnj@users.noreply.github.com>
2025-08-04 10:30:26 +0330

    Create Functions #3 - simple Boolean.py

---
+++ 6. Methods and Functions/Functions #3 - simple Boolean.py
@@ -0,0 +1,10 @@
+'''
+Define a function called myfunc that takes in a Boolean value (True or False). If True, return 'Hello', and if False, return 'Goodbye'
+'''
+
+def myfunc(boolean):
+    if boolean == True:
+        return "Hello"
+    else:
+        return "Goodbye"
+    
