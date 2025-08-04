8e542c529535a112f7381ea174dd535d38763b70
Mahshad Nazari Jeirani <88314947+mahshadnj@users.noreply.github.com>
2025-08-04 10:45:56 +0330

    Create Functions #7: is greater.py

---
+++ 6. Methods and Functions/Functions #7: is greater.py
@@ -0,0 +1,9 @@
+'''
+Define a function called is_greater that takes in two arguments, and returns True if the first value is greater than the second, False if it is less than or equal to the second.
+'''
+
+def is_greater(a,b):
+    if a>b:
+        return True
+    else:
+        return False
