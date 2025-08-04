2ec8a608f2aa25a638aef0d3358caa6992d91f4b
Mahshad Nazari Jeirani <88314947+mahshadnj@users.noreply.github.com>
2025-08-04 10:45:13 +0330

    Create Functions #6: is even.py

---
+++ 6. Methods and Functions/Functions #6: is even.py
@@ -0,0 +1,10 @@
+'''
+Functions #6: is even
+Define a function called is_even that takes in one argument, and returns True if the passed-in value is even, False if it is not.
+'''
+
+def is_even(x):
+    if x%2==0:
+        return True
+    else:
+        return False
